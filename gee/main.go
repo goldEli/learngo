74d74a12efac699a3c2debfa3af855cfbb0e0355
miaoyu <miaoyu2009@qq.com>
2021-10-17 16:35:20 +0800

    http get

--- gee/main.go
+++ gee/main.go
@@ -2,18 +2,41 @@ package gee
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
+type HandlerFunc func(http.ResponseWriter, *http.Request)
 type Engine struct {
+	router map[string]HandlerFunc
+}
+
+func New() *Engine {
+	return &Engine{router: make(map[string]HandlerFunc)}
+}
+
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRouter("GET", pattern, handler)
+}
+
+func (engine *Engine) Run(port string) {
+	log.Fatal(http.ListenAndServe(port, engine))
+}
+
+func (engine *Engine) addRouter(method string, pattern string, handler HandlerFunc) {
+	key := method + "_" + pattern
+	engine.router[key] = handler
 }
 
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
-	switch r.URL.Path {
-	case "/":
-		fmt.Fprintf(w, "url = %q\n", r.URL.Path)
-	default:
-		fmt.Fprintf(w, "404 not fount: %q\n", r.URL.Path)
+	method := r.Method
+	pattern := r.URL.Path
+	key := method + "_" + pattern
+
+	if handler, ok := engine.router[key]; ok {
+		handler(w, r)
+	} else {
+		fmt.Fprintf(w, "404 NOT FOUND: %q:%q\n", method, pattern)
 	}
 }
--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"net/http"
 
 	"github.com/goldEli/learngo/gee"
 )
 
 func main() {
-	engine := new(gee.Engine)
-
-	log.Fatal(http.ListenAndServe(":9999", engine))
+	engine := gee.New()
+	engine.GET("/", func(rw http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(rw, "/: hello")
+	})
+	engine.Run(":9999")
 }
