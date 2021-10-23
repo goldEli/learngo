ff3a63d0e4c7b343d83e398fc78e007f7b767974
miaoyu <miaoyu2009@qq.com>
2021-10-23 14:52:15 +0800

    ✨ gee http static server

--- gee/group.go
+++ gee/group.go
@@ -1,12 +1,38 @@
 package gee
 
-import "log"
+import (
+	"log"
+	"net/http"
+	"path"
+)
 
 type RouterGroup struct {
 	prefix      string
 	parent      *RouterGroup
 	engine      *Engine
 	middlewares []HandlerFunc
+}
+
+func (group *RouterGroup) createStaticHandler(relativePath string, fs http.FileSystem) HandlerFunc {
+	absolutePath := path.Join(group.prefix, relativePath)
+	fileServer := http.StripPrefix(absolutePath, http.FileServer((fs)))
+	return func(c *Context) {
+		file := c.Param("filepath")
+
+		if _, err := fs.Open(file); err != nil {
+			c.Status(http.StatusNotFound)
+			return
+		}
+
+		fileServer.ServeHTTP(c.Writer, c.Req)
+	}
+}
+
+func (group *RouterGroup) Static(relativePath string, root string) {
+	handler := group.createStaticHandler(relativePath, http.Dir(root))
+	urlPattern := path.Join(relativePath, "/*filepath")
+
+	group.GET(urlPattern, handler)
 }
 
 func (group *RouterGroup) Use(middleware ...HandlerFunc) {
