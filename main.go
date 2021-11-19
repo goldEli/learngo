692b006bbb2e7277955a8b0adb717392ab531e8a
miaoyu <miaoyu2009@qq.com>
2021-11-19 16:04:13 +0800

    feat: hash consitent

--- main.go
+++ main.go
@@ -2,30 +2,37 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
-
-	"github.com/goldEli/learngo/geecache"
+	"strconv"
 )
 
-var db = map[string]string{
-	"tom":  "43",
-	"lily": "53",
-	"sam":  "63",
+func queryUserById(id int) chan string {
+	c := make(chan string)
+	go func() {
+		c <- "姓名" + strconv.Itoa(id)
+	}()
+	return c
 }
 
 func main() {
-	key := "scores"
-	geecache.NewGroup(key, 2<<10, geecache.GetterFunc(func(key string) ([]byte, error) {
-		log.Println("[SlowDB] search key", key)
-		if v, ok := db[key]; ok {
-			return []byte(v), nil
+	c1, c2, c3 := queryUserById(1), queryUserById(2), queryUserById(3)
+	c := make(chan string)
+	// 开一个goroutine监视各个信道数据输出并收集数据到信道c
+	go func() {
+		for {
+			// 监视c1, c2, c3的流出，并全部流入信道c
+			select {
+			case v1 := <-c1:
+				c <- v1
+			case v2 := <-c2:
+				c <- v2
+			case v3 := <-c3:
+				c <- v3
+			}
 		}
-		return nil, fmt.Errorf("%s not exist", key)
-	}))
-	addr := "localhost:9999"
-	peers := geecache.NewHTTPPool(addr)
-	log.Println("geecache is running at", addr)
-	log.Fatal(http.ListenAndServe(addr, peers))
-
+	}()
+	// 阻塞主线，取出信道c的数据
+	for i := 0; i < 3; i++ {
+		// 从打印来看我们的数据输出并不是严格的顺序
+		fmt.Println(<-c)
+	}
 }
